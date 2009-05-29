svn2github/rkward-svn-mirror

commit 5e61fcf9ecc8390176316aabb6e7527c609d889a

Add check for matching version of rkward R library
git-svn-id: https://svn.code.sf.net/p/rkward/code@2508 7936002b-cd1d-0410-9ce3-d7ebf93b5162

trunk/rkward/rkward/rbackend/rthread.cpp
@@ -22,6 +22,7 @@
 #include "../settings/rksettingsmodulegeneral.h"
 #include "../rkglobals.h"
 #include "../rkward.h"		// for startup options
+#include "../version.h"
 
 #include "../debug.h"
 
@@ -78,12 +79,14 @@ void RThread::run () {
 	// in RInterface::RInterface() we create a fake RCommand to capture all the output/errors during startup
 	MUTEX_LOCK;
 	current_command = RCommandStack::regular_stack->currentCommand ();
+	all_current_commands.append (current_command);
 	RK_ASSERT (current_command);
 	MUTEX_UNLOCK;
 
 	if ((err = initialize ())) {
 		int* err_c = new int;
 		*err_c = err;
+		flushOutput ();		// to make errors/warnings available to the main thread
 		qApp->postEvent (RKGlobals::rInterface (), new RKRBackendEvent (RKRBackendEvent::RStartupError, err_c));
 	}
 	qApp->postEvent (RKGlobals::rInterface (), new RKRBackendEvent (RKRBackendEvent::RStarted));
@@ -509,6 +512,8 @@ int RThread::initialize () {
 	int status = 0;
 	
 	runCommandInternal ("library (\"rkward\")\n", &error);
+	if (error) status |= LibLoadFail;
+	runCommandInternal (QString ("stopifnot(.rk.app.version==\"%1\")\n").arg (VERSION), &error);
 	if (error) status |= LibLoadFail;
 
 // find out about standard library locations

trunk/rkward/makedist.sh
@@ -7,10 +7,10 @@ DISTDIRREL=rkward-$VERSION
 DISTDIR=$BASEDIR/disttemp/$DISTDIRREL
 mkdir $DISTDIR
 
-#prepare version.h
+#prepare version information
 echo "/* Version number of package */" > $BASEDIR/rkward/version.h
 echo "#define VERSION \"$VERSION\"" >> $BASEDIR/rkward/version.h
-#prepare resource.ver
+echo "\".rk.app.version\" <- \"$VERSION\"" > $BASEDIR/rkward/rbackend/rpackages/rkward/R/ver.R
 echo "$VERSION" > $BASEDIR/rkward/resource.ver
 
 cp -a AUTHORS CMakeLists.txt COPYING ChangeLog TODO INSTALL NOTES README configure $DISTDIR

trunk/rkward/rkward/rbackend/rinterface.cpp
@@ -204,18 +204,24 @@ void RInterface::customEvent (QEvent *e) {
 		int* err_p = static_cast<int*> (ev->data ());
 		int err = *err_p;
 		delete err_p;
-		QString message = i18n ("There was a problem starting the R backend. The following error(s) occurred:\n");
+		QString message = i18n ("<p>There was a problem starting the R backend. The following error(s) occurred:</p>\n");
 		if (err & RThread::LibLoadFail) {
-			message.append (i18n ("\t- The 'rkward' R-library could not be loaded. This library is needed for communication between R and RKWard and many things will not work properly if this library is not present. Likely RKWard will even crash. The 'rkward' R-library should have been included in your distribution or RKWard, and should have been set up when you ran 'make install'. Please try 'make install' again and check for any errors. You should quit RKWard now.\n"));
+			message.append (i18n ("</p>\t- The 'rkward' R-library either could not be loaded at all, or not in the correct version. This may lead to all sorts of errors, from single missing features to complete failure to function. The most likely cause is that the last installation did not place all files in the correct place. However, in some cases, left-overs from a previous installation that was not cleanly removed may be the cause.</p>\
+			<p><b>You should quit RKWard, now, and fix your installation</b>. For help with that, see <a href=\"http://p.sf.net/rkward/compiling\">http://p.sf.net/rkward/compiling</a>.</p>\n"));
 		}
 		if (err & RThread::SinkFail) {
-			message.append (i18n ("\t-There was a problem opening the files needed for communication with R. Most likely this is due to an incorrect setting for the location of these files. Check whether you have correctly configured the location of the log-files (Settings->Configure Settings->Logfiles) and restart RKWard.\n"));
+			message.append (i18n ("<p>\t-There was a problem opening the files needed for communication with R. Most likely this is due to an incorrect setting for the location of these files. Check whether you have correctly configured the location of the log-files (Settings->Configure Settings->Logfiles) and restart RKWard.</p>\n"));
 		}
 		if (err & RThread::OtherFail) {
-			message.append (i18n ("\t-An unspecified error occurred that is not yet handled by RKWard. Likely RKWard will not function properly. Please check your setup.\n"));
+			message.append (i18n ("<p>\t-An unspecified error occurred that is not yet handled by RKWard. Likely RKWard will not function properly. Please check your setup.</p>\n"));
 		}
-		KMessageBox::error (0, message, i18n ("Error starting R"));
-		r_thread->unlock (RThread::Startup);
+		QString details = runningCommand()->fullOutput();
+		if (!details.isEmpty ()) {
+			// WORKAROUND for stupid KMessageBox behavior. (kdelibs 4.2.3)
+			// If length of details <= 512, it tries to show the details as a QLabel.
+			details = details.replace('<', "&lt;").replace('\n', "<br>").leftJustified (513);
+		}
+		KMessageBox::detailedError (0, message, details, i18n ("Error starting R"), KMessageBox::Notify | KMessageBox::AllowLink);
 	} else {
 		RK_ASSERT (false);
 	}
@@ -380,7 +386,7 @@ void RInterface::processREvalRequest (REvalRequest *request) {
 			bool ok;
 			RKComponentMap::ComponentInvocationMode mode = RKComponentMap::ManualSubmit;
 			if (request->call[2] == "auto") mode = RKComponentMap::AutoSubmit;
-			else if (request->call[2] == "submit.or.fail") mode = RKComponentMap::AutoSubmitOrFail;
+			else if (request->call[2] == "submit") mode = RKComponentMap::AutoSubmitOrFail;
 			ok = RKComponentMap::invokeComponent (request->call[1], request->call.mid (3), mode, &message);
 
 			if (message.isEmpty ()) {

trunk/rkward/rkward/rbackend/rpackages/rkward/R/public.R
@@ -320,7 +320,7 @@
 	.Call ("rk.edit.files", file, title, name)
 }
 
-"rk.call.plugin" <- function (plugin, ..., submit.mode = c ("manual", "auto", "submit.or.fail")) {
+"rk.call.plugin" <- function (plugin, ..., submit.mode = c ("manual", "auto", "submit")) {
 	# prepare arguments
 	settings <- list (...)
 	callstrings <- list ()

trunk/rkward/rkward/rbackend/rpackages/rkward/R/ver.R
@@ -1,0 +1,1 @@
+".rk.app.version" <- "0.5.1-pre1"

trunk/rkward/techdocs/release_recipe
@@ -1,11 +1,5 @@
-This procedure helps working around a number of strange problems while creating releases, such as .moc-files not being generated, generated files not being removed, autotools being run again (with bad results) for no good reason... The following procedure seems to eliminate those problems, and thus should be followed for releases. Some items may also serve to keep the package lean.
+Since RKWard 0.5.0 and the use of cmake, creating a new source .tar.gz is as easy as:
 
-make clean
-make dist
-unpack tarball and cd there
-rm -rf admin/.svn
-make -f Makefile.svn
-rm rkward/rkward_skel.cpp
-touch config.h.in
-rm -r autom4te.cache
-repackage tarball
+./makedist.sh VERSION
+
+in the toplevel source directory. Be sure to keep your sources clean, when producing official releases, tough. (If in doubt, get a clean SVN-checkout, and use that).

commit 0bba867bec5daa4d783f0443112c3003b74151a7

Fix for crash caused by r2508
git-svn-id: https://svn.code.sf.net/p/rkward/code@2509 7936002b-cd1d-0410-9ce3-d7ebf93b5162

trunk/rkward/rkward/rbackend/rthread.cpp
@@ -99,6 +99,7 @@ void RThread::run () {
 	MUTEX_LOCK;
 	checkObjectUpdatesNeeded (true);
 	RCommandStack::regular_stack->pop ();	// remove the fake command
+	all_current_commands.pop_back ();
 	notifyCommandDone (current_command);
 	MUTEX_UNLOCK;
 